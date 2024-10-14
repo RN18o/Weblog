3a183fe61116c9daf39b669026e4ef4fd98f3205
Aryan Chaurasiya <aryanchaurasia1806@gmail.com>
2024-10-14 17:20:24 +0530

    Next Js

---
+++ app/blog/[id]/page.jsx
@@ -0,0 +1,196 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import moment from "moment";
+import { AiTwotoneCalendar } from "react-icons/ai";
+import { BsFillPencilFill, BsTrash } from "react-icons/bs";
+import demoImage from "@/images/demo_image.jpg";
+import { deletePhoto } from "@/actions/uploadActions";
+
+function splitParagraph(paragraph) {
+  const MIN_LENGTH = 280;
+  const sentences = paragraph.split(". ");
+
+  let currentParagraph = "";
+  let paragraphs = [];
+
+  for (let i = 0; i < sentences.length; i++) {
+    const sentence = sentences[i];
+    const isLastSentence = i === sentences.length - 1;
+
+    if (isLastSentence) {
+      currentParagraph += sentence + " "; // No dot after the last sentence
+    } else if (currentParagraph.length + sentence.length + 2 <= MIN_LENGTH) {
+      currentParagraph += sentence + ". ";
+    } else {
+      paragraphs.push(<p key={paragraphs.length}>{currentParagraph.trim()}</p>);
+      currentParagraph = sentence + ". ";
+    }
+  }
+
+  if (currentParagraph) {
+    paragraphs.push(<p key={paragraphs.length}>{currentParagraph.trim()}</p>);
+  }
+
+  return paragraphs;
+}
+
+const BlogDetails = ({ params }) => {
+  const [blogDetails, setBlogDetails] = useState({});
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  async function fetchBlog() {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/blog/${params.id}`
+      );
+      const blog = await response.json();
+      setBlogDetails(blog);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+
+  const timeStr = blogDetails?.createdAt;
+  const time = moment(timeStr);
+  const formattedTime = time.format("MMMM Do YYYY");
+
+  const handleBlogDelete = async (imageId) => {
+    try {
+      const confirmModal = window.confirm(
+        "Are you sure you want to delete your blog?"
+      );
+
+      if (confirmModal) {
+        setIsDeleting(true);
+        const response = await fetch(
+          `http://localhost:3000/api/blog/${params.id}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${session?.user?.accessToken}`,
+            },
+          }
+        );
+
+        if (response?.status === 200) {
+          await deletePhoto(imageId);
+          router.refresh();
+          router.push("/blog");
+        }
+      }
+
+      setIsDeleting(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <section className="container max-w-3xl">
+      {blogDetails?.authorId?._id.toString() ===
+        session?.user?._id.toString() && (
+        <div className="flex items-center justify-end gap-5">
+          <Link
+            href={`/blog/edit/${params.id}`}
+            className="flex items-center gap-1 text-primaryColor"
+          >
+            <BsFillPencilFill />
+            Edit
+          </Link>
+
+          <button
+            onClick={() => handleBlogDelete(blogDetails?.image?.id)}
+            className="flex items-center gap-1 text-red-500"
+          >
+            <BsTrash />
+            {isDeleting ? "Deleting..." : "Delete"}
+          </button>
+        </div>
+      )}
+
+      <div className="flex flex-col items-center justify-center">
+        <Link href={`/user/${blogDetails?.authorId?._id.toString()}`}>
+          <div className="flex flex-col justify-center items-center py-10">
+            <Image
+              src={
+                blogDetails?.authorId?.avatar?.url
+                  ? blogDetails?.authorId?.avatar?.url
+                  : demoImage
+              }
+              alt="avatar image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-20 h-20 rounded-full"
+            />
+
+            <div className="text-center">
+              <p className="text-whiteColor">{blogDetails?.authorId?.name}</p>
+              <p>{blogDetails?.authorId?.designation}</p>
+            </div>
+          </div>
+        </Link>
+
+        <div className="text-center space-y-3">
+          <h2>{blogDetails?.title}</h2>
+
+          <p>{blogDetails?.excerpt}...</p>
+
+          <p className="flex items-center justify-center gap-3">
+            <span className="text-primaryColor">{blogDetails?.category}</span>
+
+            <span className="flex items-center gap-1">
+              <AiTwotoneCalendar />
+              {formattedTime}
+            </span>
+          </p>
+
+          <div>
+            <Image
+              src={blogDetails?.image ? blogDetails?.image?.url : demoImage}
+              alt="blog details image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-full rounded-lg py-10"
+            />
+          </div>
+
+          <div className="text-start">
+            <div className="space-y-5">
+              {blogDetails?.description &&
+                splitParagraph(blogDetails?.description).map(
+                  (paragraph, pIndex) => (
+                    <div key={pIndex}>
+                      {pIndex ===
+                        Math.floor(
+                          splitParagraph(blogDetails?.description).length / 2
+                        ) && (
+                        <blockquote className="border-l-4 border-primaryColor border-spacing-6 italic mb-5">
+                          <p className="ml-5">{blogDetails?.quote}</p>
+                        </blockquote>
+                      )}
+
+                      {paragraph}
+                    </div>
+                  )
+                )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlogDetails;
--- app/page.js
+++ app/page.js
@@ -1,101 +1,33 @@
 import Image from "next/image";
+import blog from "@/images/home.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="container flex flex-col md:flex-row gap-5 h-[calc(100vh-4rem)]">
+      <div className="basis-full flex flex-col justify-center md:basis-2/3">
+        <p className="special-word text-xs">Write and read blog</p>
+        <h1 className="pb-5">
+          Write Your <span className="special-word">Thoughts</span>
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <p>
+          Hii there! We provide one of the original and highest quality blog for
+          tech and Enthusiasts industry. Read high quality blogs for free and
+          just create an account and write your own blog.
+        </p>
+      </div>
+
+      <div className="hidden md:block basis-1/3">
+        <Image
+          src={blog}
+          alt="blog"
+          // fill
+          // sizes="100vw"
+          // width={500}
+          // height={500}
+          className=" mt-64"
+        />
+      </div>
     </div>
   );
 }
